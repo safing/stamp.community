ca003da1a917871955ffcf235353bf14fc26701b
davegson <3080765+davegson@users.noreply.github.com>
2017-11-08 16:09:09 +0100

    Improve stamps factory

--- spec/factories/stamps.rb
+++ spec/factories/stamps.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     downvote_count 0
     label
     creator_id { create(:user).id }
-    stampable_type 'Domain'
-    stampable_id { create(:domain).id }
+    association :stampable, factory: :domain
   end
 end
