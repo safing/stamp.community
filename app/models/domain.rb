b0d869f1d9c03713c17bbfd2ba1ceb2fc18defe8
davegson <3080765+davegson@users.noreply.github.com>
2017-11-08 16:33:23 +0100

    Fix Domain relationship to Stamp

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,5 +2,5 @@ class Domain < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   belongs_to :parent, class_name: 'Domain'
   has_many :children, class_name: 'Domain', foreign_key: 'parent_id'
-  belongs_to :stamp
+  has_many :stamps, as: :stampable
 end
