b5cabf04bdcd71a489f0f17c32fe6c94ba54b24d
davegson <3080765+davegson@users.noreply.github.com>
2018-02-16 16:31:10 +0100

    Add Vote#power

--- db/migrate/20171108145836_create_votes.rb
+++ db/migrate/20171108145836_create_votes.rb
@@ -4,6 +4,7 @@ class CreateVotes < ActiveRecord::Migration[5.1]
       t.references :votable, polymorphic: true, null: false
       t.references :user, foreign_key: true, null: false
       t.string :type, null: false
+      t.integer :power, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20171113154430) do
 
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.integer "power", null: false
     t.string "type", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
