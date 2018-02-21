01945b6d48e5d39b72150acde0159d6834817a88
davegson <3080765+davegson@users.noreply.github.com>
2018-02-21 12:48:09 +0100

    Set the users voting_power when at the time where he votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,14 @@
 class Vote < ApplicationRecord
+  before_validation :cache_users_voting_power, on: :create
+
   belongs_to :votable, polymorphic: true
   belongs_to :user
 
   validates_presence_of :power, :user, :votable
+
+  private
+
+  def cache_users_voting_power
+    self.power = user.voting_power
+  end
 end
---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+RSpec.describe Vote do
+  describe '#create' do
+    subject { vote.save }
+    let(:vote) { FactoryBot.build(:vote) }
+
+    it 'calls #cache_users_voting_power' do
+      expect(vote).to receive(:cache_users_voting_power)
+      subject
+    end
+
+    it 'creates a vote' do
+      expect { subject }.to change { Vote.count }.from(0).to(1)
+    end
+  end
+
+  describe '#save' do
+    subject { vote.save }
+
+    context 'vote is not a new record' do
+      let(:vote) { FactoryBot.create(:vote) }
+
+      it 'does not call #cache_users_voting_power' do
+        expect(vote).not_to receive(:cache_users_voting_power)
+        subject
+      end
+    end
+  end
+
+  describe '#cache_users_voting_power' do
+    subject { vote.send('cache_users_voting_power') }
+    let(:vote) { FactoryBot.build(:vote) }
+
+    it 'saves the users voting power as vote#power' do
+      expect { subject }.to change { vote.power }.from(nil).to(vote.user.voting_power)
+    end
+  end
+end
