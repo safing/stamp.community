e9fff65bb60893fbd3422edc5c89e7baaac59750
davegson <3080765+davegson@users.noreply.github.com>
2018-02-21 13:21:47 +0100

    Validate Stamp#percentage

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -2,5 +2,7 @@ class Stamp < ApplicationRecord
   include Votable
   include Votable::State
 
+  validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
+
   belongs_to :label
 end
