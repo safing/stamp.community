4dc806a5c18741237a49fecf5bc27e24d93bdce9
davegson <3080765+davegson@users.noreply.github.com>
2018-02-22 13:47:38 +0100

    Remove comment

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,6 @@ Bundler.require(*Rails.groups)
 
 module StampCommunity
   class Application < Rails::Application
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run 'rake -D time' for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
     config.generators do |g|
       g.test_framework :rspec
     end
