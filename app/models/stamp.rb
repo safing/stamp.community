2cc2504c69c02bc5ba03b501e245065606ac577f
davegson <3080765+davegson@users.noreply.github.com>
2018-02-23 11:06:55 +0100

    Add stamp validations

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -3,6 +3,7 @@ class Stamp < ApplicationRecord
   include Votable::State
 
   validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
+  validates_presence_of :creator, :label, :percentage, :stampable, :state
 
   belongs_to :label
 end
