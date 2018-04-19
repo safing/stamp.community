d5b98fb33f83101a03fe6e94e2e53fd04dbeaa48
davegson <3080765+davegson@users.noreply.github.com>
2018-04-19 11:35:45 +0200

    Add regex to validate domain names

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,4 +1,9 @@
 class Domain < ApplicationRecord
+  # rubocop:disable LineLength
+  # got the regex from https://stackoverflow.com/a/26987741/2235594
+  NAME_REGEX = /(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30}\.[a-z]{2,})/
+  # rubocop:enable LineLength
+
   belongs_to :creator, class_name: 'User'
   belongs_to :parent, class_name: 'Domain'
   has_many :children, class_name: 'Domain', foreign_key: 'parent_id'
