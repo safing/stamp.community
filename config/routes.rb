4cf10827ba8bf92993a6d8441d13be3240dadb67
davegson <3080765+davegson@users.noreply.github.com>
2018-05-01 13:54:56 +0200

    Use the custom registrations controller for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   mount APIRouter => '/'
 
   root to: 'application#index'
