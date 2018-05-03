7308144b3c11e48867d713658fadba0a72c055c6
davegson <3080765+davegson@users.noreply.github.com>
2018-05-03 10:17:36 +0200

    Set up name as primary key of Domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,4 +1,6 @@
 class Domain < ApplicationRecord
+  self.primary_key = 'name'
+
   # rubocop:disable LineLength
   # got the regex from https://stackoverflow.com/a/26987741/2235594
   NAME_REGEX = /(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30}\.[a-z]{2,})/
@@ -8,6 +10,8 @@ class Domain < ApplicationRecord
   belongs_to :parent, class_name: 'Domain'
   has_many :children, class_name: 'Domain', foreign_key: 'parent_id'
   has_many :stamps, as: :stampable
+
+  validates :name, format: { with: NAME_REGEX, message: 'invalid domain name' }
 
   def parent_name
     parent.name if parent_id.present?
