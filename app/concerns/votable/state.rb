0938aadf76e40d6423dc6a277c7ec4f52ae66f9e
davegson <3080765+davegson@users.noreply.github.com>
2018-05-03 15:00:48 +0200

    Add state scopes for Votable

--- app/concerns/votable/state.rb
+++ app/concerns/votable/state.rb
@@ -3,6 +3,12 @@ module Votable
     extend ActiveSupport::Concern
 
     included do
+      scope :accepted, -> { where(state: 'accepted') }
+      scope :archived, -> { where(state: 'archived') }
+      scope :denied, -> { where(state: 'denied') }
+      scope :in_progress, -> { where(state: 'in_progress') }
+      scope :overruled, -> { where(state: 'overruled') }
+
       state_machine initial: :in_progress do
         transition in_progress: :accepted, on: :accept
         transition in_progress: :denied, on: :deny
