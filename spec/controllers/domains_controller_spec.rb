679c921a22beb8d950e5ff455249b078906d857a
davegson <3080765+davegson@users.noreply.github.com>
2018-05-04 12:17:16 +0200

    Fix require calls in specs

--- spec/controllers/domains_controller_spec.rb
+++ spec/controllers/domains_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe DomainsController, type: :controller do
 end
--- spec/controllers/stamps_controller_spec.rb
+++ spec/controllers/stamps_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe StampsController, type: :controller do
 end
