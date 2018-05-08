378940b68bc9e2ebfb467e3f8553e6adc0bdec4f
davegson <3080765+davegson@users.noreply.github.com>
2018-05-08 10:23:20 +0200

    Add relation specs to Label

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,4 @@
 class Label < ApplicationRecord
-  belongs_to :parent, class_name: 'Label'
+  belongs_to :parent, class_name: 'Label', optional: true
   has_many :stamps
 end
--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,2 +1,9 @@
 RSpec.describe Label, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryBot.create(:label)).to be_valid
+  end
+
+  describe 'relations' do
+    it { is_expected.to belong_to(:parent).class_name('Label').optional }
+  end
 end
