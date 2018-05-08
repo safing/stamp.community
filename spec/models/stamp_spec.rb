6842db9366dac746c916a596c03480b193b2f190
davegson <3080765+davegson@users.noreply.github.com>
2018-05-08 12:30:33 +0200

    Add relation and validation specs for Stamp

--- spec/models/stamp_spec.rb
+++ spec/models/stamp_spec.rb
@@ -1,4 +1,20 @@
 RSpec.describe Stamp, type: :model do
+  it_behaves_like 'a votable model'
+
+  describe 'relations' do
+    it { is_expected.to belong_to(:label).required(true) }
+  end
+
+  describe 'validations' do
+    it do
+      is_expected.to validate_numericality_of(:percentage)
+        .is_greater_than(0)
+        .is_less_than_or_equal_to(100)
+    end
+    it { is_expected.to validate_presence_of(:percentage) }
+    it { is_expected.to validate_presence_of(:state) }
+  end
+
   describe '#domain?' do
     subject { stamp.domain? }
     let(:stamp) { FactoryBot.create(:stamp) }
