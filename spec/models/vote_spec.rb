451847a6b886ffeeae3cd2deced9309293ae45cd
davegson <3080765+davegson@users.noreply.github.com>
2018-05-08 12:34:46 +0200

    Add database specs for Vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe Vote, type: :model do
     it { is_expected.to validate_presence_of(:power) }
   end
 
+  describe 'database' do
+    it { is_expected.to have_db_index(:user_id) }
+    it { is_expected.to have_db_index(%i[votable_type votable_id]) }
+  end
+
   describe '#create' do
     subject { vote.save }
     let(:vote) { FactoryBot.build(:vote) }
