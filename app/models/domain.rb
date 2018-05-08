3b8be93840c79bbc09ec3619a0406e765ec27cdc
davegson <3080765+davegson@users.noreply.github.com>
2018-05-08 11:34:13 +0200

    Add domain specs

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,14 +2,15 @@ class Domain < ApplicationRecord
   # rubocop:disable LineLength
   # got the regex from https://stackoverflow.com/a/26987741/2235594
   NAME_REGEX = /(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30}\.[a-z]{2,})/
+  NAME_REGEX_WITH_ANCHORS = /\A(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30}\.[a-z]{2,})\z/
   # rubocop:enable LineLength
 
   belongs_to :creator, class_name: 'User'
-  belongs_to :parent, class_name: 'Domain'
+  belongs_to :parent, class_name: 'Domain', optional: true
   has_many :children, class_name: 'Domain', foreign_key: 'parent_id'
   has_many :stamps, as: :stampable
 
-  validates :name, format: { with: NAME_REGEX, message: 'invalid domain name' }
+  validates :name, format: { with: NAME_REGEX_WITH_ANCHORS, message: 'invalid domain name' }
 
   def parent_name
     parent.name if parent_id.present?
---
+++ spec/models/domain_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe Domain, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryBot.create(:domain)).to be_valid
+  end
+
+  describe 'relations' do
+    it { is_expected.to belong_to(:creator).class_name('User').required(true) }
+    it { is_expected.to belong_to(:parent).class_name('Domain').optional }
+    it { is_expected.to have_many(:children).class_name('Domain').with_foreign_key(:parent_id) }
+    it { is_expected.to have_many(:stamps) }
+  end
+
+  describe 'validations' do
+    describe 'domain name validation' do
+      it { is_expected.to allow_value('i.oh1.me').for(:name) }
+      it { is_expected.to allow_value('assets.fb.com').for(:name) }
+      it { is_expected.to allow_value('uni.ac.uk').for(:name) }
+      it { is_expected.to allow_value('www.uni.ac.uk').for(:name) }
+      it { is_expected.to allow_value('x.com').for(:name) }
+      it { is_expected.to allow_value('www.xn--cybr-noa.space').for(:name) }
+
+      it { is_expected.not_to allow_value('i.ehthe.oh1.me.').for(:name) }
+      it { is_expected.not_to allow_value('e//assets.fb.com').for(:name) }
+      it { is_expected.not_to allow_value('uni.').for(:name) }
+      it { is_expected.not_to allow_value('.uni').for(:name) }
+      it { is_expected.not_to allow_value('x.com/oahethutoa').for(:name) }
+      it { is_expected.not_to allow_value(')*)xn--cybr-noa.space').for(:name) }
+    end
+  end
+
+  describe 'database' do
+    it { is_expected.to have_db_index(:creator_id) }
+    it { is_expected.to have_db_index(:name).unique(true) }
+    it { is_expected.to have_db_index(:parent_id) }
+  end
+end
