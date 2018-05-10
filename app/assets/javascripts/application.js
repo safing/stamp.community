1bba746b19431de35a37d22ba45b017d800f198f
davegson <3080765+davegson@users.noreply.github.com>
2018-05-10 10:15:08 +0200

    Include bootstrap javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require turbolinks
 //= require font_awesome5
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
