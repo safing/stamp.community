532927e736c738b5823cb3756a5085182a7113d5
davegson <3080765+davegson@users.noreply.github.com>
2018-05-17 10:41:45 +0200

    Add Label#top_contributor in dummy form

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,4 +3,14 @@ class Label < ApplicationRecord
   belongs_to :parent, class_name: 'Label', optional: true
 
   has_many :stamps
+
+  # TODO
+  def top_contributors
+    User.joins(:stamps)
+        .select('stamps.creator_id, COUNT(stamps.creator_id)')
+        .where(stamps: { label_id: id })
+        .group('stamps.creator_id')
+        .order('COUNT(stamps.creator_id)')
+        .first(5)
+  end
 end
