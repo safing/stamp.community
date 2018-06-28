b01179bf260a9b19d7cdafd5bbcdaffb8e7706df
davegson <3080765+davegson@users.noreply.github.com>
2018-06-28 12:49:22 +0200

    Include warden helpers to login users for request specs

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,9 +1,31 @@
 module RequestSpecHelper
+  include Warden::Test::Helpers
+
+  def self.included(base)
+    base.before(:each) { Warden.test_mode! }
+    base.after(:each) { Warden.test_reset! }
+  end
+
+  def sign_in(resource)
+    login_as(resource, scope: warden_scope(resource))
+  end
+
+  def sign_out(resource)
+    logout(warden_scope(resource))
+  end
+
   def json
     JSON.parse(response.body)
+  end
+
+  private
+
+  def warden_scope(resource)
+    resource.class.name.underscore.to_sym
   end
 end
 
 RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.include RequestSpecHelper, type: :request
 end
