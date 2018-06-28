4eacec5d59f4780eacc1c4c453d85c7030964a55
davegson <3080765+davegson@users.noreply.github.com>
2018-06-28 14:43:26 +0200

    Add StampController#new#create with request authorization specs

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,6 +1,25 @@
 class StampsController < ApplicationController
+  def new
+    @stamp = Stamp.new
+    authorize @stamp
+  end
+
+  def create
+    @stamp = Stamp.new(stamp_params)
+    authorize @stamp
+    @stamp.save
+
+    redirect_to(stamps_path(@stamp), status: 201)
+  end
+
   def show
     @stamp = Stamp.find(params[:id])
     authorize @stamp
+  end
+
+  private
+
+  def stamp_params
+    params.require(:stamp).permit(:label_id, :percentage, :stampable_id, :stampable_type)
   end
 end
--- spec/requests/stamps_request_spec.rb
+++ spec/requests/stamps_request_spec.rb
@@ -1,6 +1,57 @@
 RSpec.describe 'stamp request', type: :request do
+  describe 'new' do
+    subject(:request) { get new_stamp_url }
+
+    describe 'authourization' do
+      context 'role: guest' do
+        include_examples 'status code', 401
+      end
+
+      context 'role: user' do
+        include_context 'login user'
+        include_examples 'status code', 200
+      end
+
+      context 'role: moderator' do
+        include_context 'login moderator'
+        include_examples 'status code', 200
+      end
+
+      context 'role: admin' do
+        include_context 'login admin'
+        include_examples 'status code', 200
+      end
+    end
+  end
+
+  describe 'create', focus: true do
+    subject(:request) { post stamps_url, params: { stamp: stamp_attributes } }
+    let(:stamp_attributes) { FactoryBot.attributes_with_foreign_keys_for(:stamp) }
+
+    describe 'authourization' do
+      context 'role: guest' do
+        include_examples 'status code', 401
+      end
+
+      context 'role: user' do
+        include_context 'login user'
+        include_examples 'status code', 201
+      end
+
+      context 'role: moderator' do
+        include_context 'login moderator'
+        include_examples 'status code', 201
+      end
+
+      context 'role: admin' do
+        include_context 'login admin'
+        include_examples 'status code', 201
+      end
+    end
+  end
+
   describe 'show' do
-    subject(:request) { get stamp_url(stamp.id) }
+    subject(:request) { get stamps_path(stamp) }
 
     let(:stamp) { FactoryBot.create(:stamp) }
 
