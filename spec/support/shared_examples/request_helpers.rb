d865e4cd5ca7ca0d32328564071c90abd86a8ad3
davegson <3080765+davegson@users.noreply.github.com>
2018-06-28 13:56:46 +0200

    Add shared contexts to login different user roles

--- spec/support/shared_examples/request_helpers.rb
+++ spec/support/shared_examples/request_helpers.rb
@@ -9,3 +9,15 @@ shared_examples_for 'status code' do |status|
     expect(response).to have_http_status(status)
   end
 end
+
+shared_context 'login user' do
+  before { sign_in(FactoryBot.create(:user)) }
+end
+
+shared_context 'login moderator' do
+  before { sign_in(FactoryBot.create(:moderator)) }
+end
+
+shared_context 'login admin' do
+  before { sign_in(FactoryBot.create(:admin)) }
+end
