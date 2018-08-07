55760a3f26669fdfe45a85f0ba2098eeb6143a0b
davegson <3080765+davegson@users.noreply.github.com>
2018-08-07 16:48:50 +0200

    Add empty views for Astralof to design

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -2,4 +2,8 @@ class StampsController < ApplicationController
   def show
     @stamp = Stamp.find(params[:id])
   end
+
+  def new; end
+
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  resources :domains, param: :name, constraints: { name: Domain::NAME_REGEX }, only: [:show]
+  resources :domains, param: :name, constraints: { name: Domain::NAME_REGEX }, only: %i[show new]
   resources :labels, only: [:show]
   resources :licences, only: [:show]
-  resources :stamps, only: [:show]
+  resources :stamps, except: %i[edit update delete]
   resources :users, only: [:show]
 
   mount APIRouter => '/'
