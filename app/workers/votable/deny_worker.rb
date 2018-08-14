b4d05debd71df740d924e2ba2c75f67be0f2f34b
davegson <3080765+davegson@users.noreply.github.com>
2018-08-14 12:33:46 +0200

    Add Votable::DenyWorker

---
+++ app/workers/votable/deny_worker.rb
@@ -0,0 +1,10 @@
+class Votable::DenyWorker
+  include Sidekiq::Worker
+
+  def perform(votable_type:, votable_id:)
+    votable = votable_type.constantize.find(votable_id)
+    raise Votable::NotInProgressError unless votable.in_progress?
+
+    votable.deny!
+  end
+end
