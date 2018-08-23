4feba0b9bc41357a846bab62caca89f78ff3a2a5
davegson <3080765+davegson@users.noreply.github.com>
2018-08-23 10:47:28 +0200

    Outsource request specs and only include authorization specs

---
+++ spec/requests/stamp_requests_spec.rb
@@ -0,0 +1,75 @@
+RSpec.feature 'stamp requests', type: :request do
+  describe 'authourization' do
+    describe '#new' do
+      subject(:request) { get new_stamp_url }
+
+      context 'role: guest' do
+        include_examples 'status code', 401
+      end
+
+      context 'role: user' do
+        include_context 'login user'
+        include_examples 'status code', 200
+      end
+
+      context 'role: moderator' do
+        include_context 'login moderator'
+        include_examples 'status code', 200
+      end
+
+      context 'role: admin' do
+        include_context 'login admin'
+        include_examples 'status code', 200
+      end
+    end
+
+    describe '#create' do
+      subject(:request) { post stamps_url, params: { stamp: stamp_attributes } }
+      let(:stamp_attributes) { FactoryBot.attributes_with_foreign_keys_for(:stamp) }
+
+      context 'role: guest' do
+        include_examples 'status code', 401
+      end
+
+      context 'role: user' do
+        include_context 'login user'
+        include_examples 'status code', 302
+      end
+
+      context 'role: moderator' do
+        include_context 'login moderator'
+        include_examples 'status code', 302
+      end
+
+      context 'role: admin' do
+        include_context 'login admin'
+        include_examples 'status code', 302
+      end
+    end
+
+    describe '#show' do
+      subject(:request) { get stamps_path(stamp) }
+
+      let(:stamp) { FactoryBot.create(:stamp) }
+
+      context 'role: guest' do
+        include_examples 'status code', 200
+      end
+
+      context 'role: user' do
+        include_context 'login user'
+        include_examples 'status code', 200
+      end
+
+      context 'role: moderator' do
+        include_context 'login moderator'
+        include_examples 'status code', 200
+      end
+
+      context 'role: admin' do
+        include_context 'login admin'
+        include_examples 'status code', 200
+      end
+    end
+  end
+end
