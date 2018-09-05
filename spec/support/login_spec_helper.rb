9f9a8bd7647c27da008a0c22fe393855e1a70f6d
davegson <3080765+davegson@users.noreply.github.com>
2018-09-05 16:10:48 +0200

    Remove superfluous LoginSpecHelper

--- spec/support/login_spec_helper.rb
+++ spec/support/login_spec_helper.rb
@@ -1,35 +1,5 @@
-module LoginSpecHelper
-  include Warden::Test::Helpers
-
-  def self.included(base)
-    base.before(:each) { Warden.test_mode! }
-    base.after(:each) { Warden.test_reset! }
-  end
-
-  def sign_in(resource)
-    login_as(resource, scope: warden_scope(resource))
-  end
-
-  def sign_out(resource)
-    logout(warden_scope(resource))
-  end
-
-  def json
-    JSON.parse(response.body)
-  end
-
-  private
-
-  def warden_scope(resource)
-    resource.class.name.underscore.to_sym
-  end
-end
-
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :feature
-  config.include Devise::Test::IntegrationHelpers, type: :view
-  config.include LoginSpecHelper, type: :request
-  config.include LoginSpecHelper, type: :feature
-  config.include LoginSpecHelper, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
