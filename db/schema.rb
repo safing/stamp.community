524714597682c82cbcecda47975a42b38cfd607f
davegson <3080765+davegson@users.noreply.github.com>
2018-10-09 15:30:47 +0200

    Add jsonb :config to Label

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002100933) do
+ActiveRecord::Schema.define(version: 20181009123636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20181002100933) do
   end
 
   create_table "labels", force: :cascade do |t|
+    t.jsonb "config", default: "{}", null: false
     t.datetime "created_at", null: false
     t.text "description"
     t.bigint "licence_id"
