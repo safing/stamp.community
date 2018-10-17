6266ed114c0674e6f8120ceea40e82e8b99ea2f3
davegson <3080765+davegson@users.noreply.github.com>
2018-10-17 15:16:15 +0200

    Remove Label#stamps_including_child_labels

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Label, type: :model do
     it { is_expected.to belong_to(:parent).class_name('Label').optional }
 
     it { is_expected.to have_many(:children).class_name('Label').with_foreign_key(:parent_id) }
-    it { is_expected.to have_many(:stamps) }
   end
 
   describe 'database' do
@@ -20,36 +19,5 @@ RSpec.describe Label, type: :model do
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:licence) }
     it { is_expected.to validate_presence_of(:name) }
-  end
-
-  describe '#stamps_including_child_labels' do
-    subject { label.stamps_including_child_labels }
-    let(:label) { FactoryBot.create(:label, :with_stamps) }
-
-    before { FactoryBot.create_list(:label_stamp, 2) }
-
-    context 'label has no child labels' do
-      it 'return all direct stamps' do
-        expect(subject.count).to eq(2)
-      end
-    end
-
-    context 'label has child labels' do
-      before do
-        FactoryBot.create_list(:label, 2, :with_stamps, parent_id: label.id)
-      end
-
-      it 'returns the labels stamps and the child labels stamps' do
-        expect(subject.count).to eq(6)
-      end
-
-      it 'returns all of the labels stamps' do
-        expect(subject.where(stamps: { label_id: label.id }).count).to eq(2)
-      end
-
-      it 'returns all of child labels stamps' do
-        expect(subject.where.not(stamps: { label_id: label.id }).count).to eq(4)
-      end
-    end
   end
 end
