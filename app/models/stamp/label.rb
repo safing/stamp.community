c5dd951290e1afd59850ee085e9e4ac81f01a3d2
davegson <3080765+davegson@users.noreply.github.com>
2018-10-17 14:49:50 +0200

    Set up basic Stamp::Label

--- app/models/stamp/label.rb
+++ app/models/stamp/label.rb
@@ -1,2 +1,15 @@
 class Stamp::Label < Stamp
+  jsonb_accessor :data, label_id: [:integer]
+  jsonb_accessor :data, percentage: [:integer]
+
+  belongs_to :label, class_name: '::Label'
+
+  validates_presence_of :label_id, :percentage
+
+  # https://stackoverflow.com/a/9463495/2235594
+  # might override stuff, a better approach might be:
+  # https://gist.github.com/sj26/5843855
+  def self.model_name
+    base_class.model_name
+  end
 end
---
+++ spec/models/stamp/label_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Stamp::Label, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryBot.create(:label_stamp)).to be_valid
+  end
+
+  describe 'relations' do
+    # shoulda-matchers does not support jsonb relations
+    xit { is_expected.to belong_to(:label).class_name('::Label') }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:label_id) }
+    it { is_expected.to validate_presence_of(:percentage) }
+  end
+end
