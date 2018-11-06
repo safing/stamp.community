2e96575a7ebd8f380489325125fff14c36f88236
davegson <3080765+davegson@users.noreply.github.com>
2018-11-06 13:18:52 +0100

    Set and validate apps operating system booleans

--- app/models/app.rb
+++ app/models/app.rb
@@ -4,7 +4,28 @@ class App < ApplicationRecord
   # https://www.devmynd.com/blog/db-generated-values-and-activerecord/
   after_create :reload
 
+  jsonb_accessor :os, (::ENVProxy.required_array('OPERATING_SYSTEMS')
+                                 .each_with_object({}) do |os, h|
+                                   h[os] = [:boolean, default: false]
+                                 end)
+
   belongs_to :user
 
   validates_presence_of %i[description link name user uuid]
+  validate :supports_one_or_more_operating_systems
+
+  def operating_systems
+    @operating_systems ||= ENVProxy.required_array('OPERATING_SYSTEMS')
+  end
+
+  def supports_one_or_more_operating_systems
+    counts = operating_systems.map { |os| send("#{os}?") }
+                              .group_by(&:itself)
+                              .transform_values(&:count)
+
+    return true if counts[true].present?
+
+    os_hash = operating_systems.each_with_object({}) { |f, h| h[f] = send(f) }
+    errors.add(:os, "- at least one operating system must be supported by the app #{os_hash}")
+  end
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -11,11 +11,85 @@ RSpec.describe App, type: :model do
     it { is_expected.to have_db_index(:user_id) }
   end
 
+  describe 'fields' do
+    let(:app) { App.new }
+
+    it '#linux is set by default, has getter and setter' do
+      expect(app.linux).to eq(false)
+      app.linux = true
+      expect(app.linux).to eq(true)
+    end
+
+    it '#macos is set by default, has getter and setter' do
+      expect(app.macos).to eq(false)
+      app.macos = true
+      expect(app.macos).to eq(true)
+    end
+
+    it '#windows is set by default, has getter and setter' do
+      expect(app.windows).to eq(false)
+      app.windows = true
+      expect(app.windows).to eq(true)
+    end
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:link) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:user) }
     it { is_expected.to validate_presence_of(:uuid) }
+
+    describe '#supports_one_or_more_operating_systems' do
+      subject { app.valid? }
+      let(:app) { FactoryBot.build(:app, **group_hash) }
+      let(:group) { %i[linux macos windows] }
+      let(:group_hash) { {} }
+
+      let(:linux) { false }
+      let(:macos) { false }
+      let(:windows) { false }
+
+      # rubocop:disable Security/Eval
+      # => { linux: false, macos: false, windows: false }
+      before { group.map { |e| group_hash[e] = eval(e.to_s) } }
+      # rubocop:enable Security/Eval
+
+      context 'no operating system is set to true' do
+        it 'returns false' do
+          expect(subject).to eq(false)
+          expect(app.errors.full_messages.first).to(
+            include('at least one operating system must be supported by the app')
+          )
+        end
+      end
+
+      context 'one operating system is set to true' do
+        let(:windows) { true }
+
+        it 'returns true' do
+          expect(subject).to eq(true)
+        end
+      end
+
+      context 'two operating systems are set to true' do
+        let(:linux) { true }
+        let(:macos) { true }
+
+        it 'returns true' do
+          expect(subject).to eq(true)
+        end
+      end
+
+      context 'all operating systems are set to true' do
+        let(:linux) { true }
+        let(:macos) { true }
+        let(:windows) { true }
+
+        it 'returns true' do
+          expect(subject).to eq(true)
+        end
+      end
+    end
   end
 end
