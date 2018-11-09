d0e23e8004990c79cb71cb2aa40edd7efa33bf67
davegson <3080765+davegson@users.noreply.github.com>
2018-11-09 09:48:06 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # rubocop:disable LineLength
   resources :domains, param: :name, constraints: { name: Domain::NAME_REGEX }, only: %i[show new create]
   # rubocop:enable LineLength
-  resources :labels, only: [:show]
+  resources :labels, only: [:show, :index]
   resources :licences, only: [:show]
   resources :stamps, except: [:delete] do
     resources :comments, only: [:create]
