afbc4687d654dc94207152c0c8da2768021a5b65
davegson <3080765+davegson@users.noreply.github.com>
2018-11-10 16:00:59 +0100

    Polish AppsController and add authorization specs

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,11 +1,29 @@
 class AppsController < ApplicationController
   def show
     @app = App.find(params[:id])
+    authorize @app
   end
 
   def new
     @app = App.new
+    authorize @app
   end
 
-  def create; end
+  def create
+    @app = App.new(app_params)
+    @app.user = current_user
+    authorize(@app)
+
+    if @app.save
+      redirect_to(app_path(@app.id), flash: { success: 'App created successfully' })
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def app_params
+    params.require(:app).permit(:name, :description, :link, :linux, :macos, :windows)
+  end
 end
