575510ed4eb0cb8b16aac0a4a2dfd3b077c414fb
davegson <3080765+davegson@users.noreply.github.com>
2018-11-10 15:58:58 +0100

    Add AppPolicy

---
+++ spec/policies/app_policy_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe AppPolicy do
+  subject { described_class.new(user, app) }
+  let(:app) { FactoryBot.create(:app) }
+
+  context 'for a visitor' do
+    let(:user) { nil }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to forbid_edit_and_update_actions }
+  end
+
+  context 'for a user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to forbid_edit_and_update_actions }
+  end
+
+  context 'for a moderator' do
+    let(:user) { FactoryBot.create(:moderator) }
+
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_edit_and_update_actions }
+  end
+
+  context 'for an admin' do
+    let(:user) { FactoryBot.create(:admin) }
+
+    it { is_expected.to permit_new_and_create_actions }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_edit_and_update_actions }
+  end
+end
