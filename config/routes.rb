0ac9e88d1ad37e08d95597bb4d2139e448b71c00
davegson <3080765+davegson@users.noreply.github.com>
2018-11-15 15:36:30 +0100

    Set up neat routes for all Stamp::TYPES

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  get 'data_stamps/show'
-  get 'data_stamps/new'
-  get 'data_stamps/create'
-
   resources :apps, only: %i[show new create]
   # rubocop:disable LineLength
   resources :domains, param: :name, constraints: { name: Domain::NAME_REGEX }, only: %i[show new create]
@@ -17,7 +13,20 @@ Rails.application.routes.draw do
   resources :licences, only: [:show]
   resources :users, only: [:show]
 
-  resources :stamps, except: [:delete] do
+  # neat patterns for different Stamp::TYPES =>
+  #  Prefix                URI Pattern                       Controller#Action
+  # flag_stamp_index       /flag_stamps(.:format)            stamps/flags#index
+  #  new_label_stamp       /label_stamps/new(.:format)       stamps/labels#new
+  Stamp::TYPES.map { |t| t.demodulize.underscore }.each do |type|
+    resources type,
+              controller: "stamps/#{type.pluralize}",
+              as: "#{type}_stamp",
+              path: "#{type}_stamps",
+              except: :destroy
+  end
+
+  # voting & commenting on a stamp makes no difference of the stamp#type, they only needs an :id
+  scope 'stamps/:id', as: :stamp do
     resources :comments, only: [:create]
     resources :votes, only: [:create]
   end
