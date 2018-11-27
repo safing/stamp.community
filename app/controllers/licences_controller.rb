0d4bd117c57ee3b4829d5a9d855d08daab79e8fe
Astralof <41782505+Astralof@users.noreply.github.com>
2018-11-27 11:18:36 +0100

    Add user and licence index

--- app/controllers/licences_controller.rb
+++ app/controllers/licences_controller.rb
@@ -2,4 +2,7 @@ class LicencesController < ApplicationController
   def show
     @licence = Licence.find(params[:id])
   end
+  def index
+    @licences = Licence.all
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def index
+    @users = User.all
+  end
 end
