5bd3d8042c91ab77e374a96cf85fd9d703a5d44e
davegson <3080765+davegson@users.noreply.github.com>
2018-12-17 14:12:26 +0100

    Configure ActionView to handle markdown files
    https://gist.github.com/davidjrice/3014948

---
+++ lib/markdown_handler.rb
@@ -0,0 +1,19 @@
+require 'redcarpet'
+
+module MarkdownHandler
+  def self.erb
+    @erb ||= ActionView::Template.registered_template_handler(:erb)
+  end
+
+  def self.call(template)
+    options = {
+      fenced_code_blocks:           true,
+      autolink:                     true
+    }
+
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    "#{@markdown.render(template.source).inspect}.html_safe"
+  end
+end
+
+ActionView::Template.register_template_handler(:md, MarkdownHandler)
