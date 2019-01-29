591b9f90b78a3bf3bb8092ee847f2d5b5bfb2bd7
davegson <3080765+davegson@users.noreply.github.com>
2019-01-29 11:26:59 +0100

    Add System model

---
+++ app/models/system.rb
@@ -0,0 +1,10 @@
+# Used for system activites. It basically enables PublicActivity to create a system activity
+# so instead of: {owner_type: User, owner_id: 5}
+# it creates: {owner_type: System, owner_id: -1}
+# enables: System.new.activities
+# => returns all system activities
+class System < User
+  def id
+    -1
+  end
+end
