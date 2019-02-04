ec6f4d6a17c29be5dcae6f9624ce8368780db341
davegson <3080765+davegson@users.noreply.github.com>
2019-02-04 16:00:22 +0100

    Configure mailer host for development & test env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://stamp.test' }
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
