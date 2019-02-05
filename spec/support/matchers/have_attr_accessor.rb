5f5a0353beca2cfe79cfb4bbfd8b7be9d6ec4b96
davegson <3080765+davegson@users.noreply.github.com>
2019-02-05 17:46:08 +0100

    Add custom matcher have_attr_accessor
    https://stackoverflow.com/a/28705325/2235594

---
+++ spec/support/matchers/have_attr_accessor.rb
@@ -0,0 +1,19 @@
+# https://stackoverflow.com/a/28705325/2235594
+RSpec::Matchers.define :have_attr_accessor do |field|
+  match do |object_instance|
+    object_instance.respond_to?(field) &&
+      object_instance.respond_to?("#{field}=")
+  end
+
+  failure_message do |object_instance|
+    "expected attr_accessor for #{field} on #{object_instance}"
+  end
+
+  failure_message_when_negated do |object_instance|
+    "expected attr_accessor for #{field} not to be defined on #{object_instance}"
+  end
+
+  description do
+    "have an attr accessor for ##{field}"
+  end
+end
