c9e1cc9b6de69db4dacc5da7eda8c892ae8a35f2
davegson <3080765+davegson@users.noreply.github.com>
2019-02-05 15:04:22 +0100

    Wrap specs around PublicActivity.with_tracking
    registration request specs are all dependent on Activities being created

--- spec/requests/users/registration_requests_spec.rb
+++ spec/requests/users/registration_requests_spec.rb
@@ -1,4 +1,10 @@
 RSpec.feature 'registration requests', type: :request do
+  # theses specs are all dependent on Activity tracking
+  around do |test|
+    PublicActivity.with_tracking do
+      test.run
+    end
+  end
   describe 'activities' do
     describe '#create' do
       subject(:request) { post user_registration_path, params: { user: user_attributes } }
@@ -7,16 +13,14 @@ RSpec.feature 'registration requests', type: :request do
       end
 
       it "creates an 'user.signed_up' activity with {owner: user, trackable: user}" do
-        PublicActivity.with_tracking do
-          expect { subject }.to change { PublicActivity::Activity.count }.from(0).to(1)
+        expect { subject }.to change { PublicActivity::Activity.count }.from(0).to(1)
 
-          user = User.last
-          activity = PublicActivity::Activity.first
-          expect(activity.key).to eq("user.signup")
-          expect(activity.owner).to eq(user)
-          expect(activity.trackable).to eq(user)
-          expect(activity.recipient).to eq(nil)
-        end
+        user = User.last
+        activity = PublicActivity::Activity.first
+        expect(activity.key).to eq("user.signup")
+        expect(activity.owner).to eq(user)
+        expect(activity.trackable).to eq(user)
+        expect(activity.recipient).to eq(nil)
       end
     end
   end
