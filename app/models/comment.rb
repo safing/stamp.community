6df8c70806a4d6db022cfd18bd5b801ccaac79c7
davegson <3080765+davegson@users.noreply.github.com>
2019-03-19 16:22:03 +0100

    Extend Comment#create_activity to also create notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,22 @@ class Comment < ApplicationRecord
 
   validates :content, length: { minimum: 40 }
   validates_presence_of %i[commentable content user]
+
+  def create_activity(*args)
+    activity = super(*args)
+
+    Notification.transaction do
+      # exclude the actor from receiving a notification
+      recipient_ids = commentable.commenter_ids - [user_id]
+      recipient_ids.each do |recipient_id|
+        Notification.create(
+          activity: activity,
+          actor_id: user_id,
+          recipient_id: recipient_id,
+          reference: commentable
+        )
+      end
+    end
+    activity
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -21,4 +21,61 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_db_index(:user_id) }
     it { is_expected.to have_db_index(%i[commentable_type commentable_id]) }
   end
+
+  describe '#create_activity(*args)' do
+    around do |test|
+      PublicActivity.with_tracking do
+        test.run
+      end
+    end
+
+    subject { comment.create_activity(*params) }
+
+    let(:comment) { FactoryBot.create(:initial_comment) }
+    let(:stamp) { Stamp.find(comment.commentable_id) }
+    let(:params) { [:create, owner: actor, recipient: stamp] }
+    let(:actor) { comment.user }
+
+    it 'creates an activity' do
+      expect { subject }.to change { PublicActivity::Activity.count }.by(1)
+    end
+
+    it 'returns the created activity' do
+      expect(subject).to be_a(PublicActivity::Activity)
+    end
+
+    context 'noone else has commented on the commentable' do
+      it 'does not create any notifications' do
+        expect { subject }.not_to change { Notification.count }
+      end
+    end
+
+    context 'others have commented on the commentable' do
+      before do
+        other_commenters.each do |commenter|
+          stamp.comments << FactoryBot.create(:comment, user: commenter)
+        end
+        stamp.save
+      end
+
+      let(:other_commenters) { FactoryBot.create_list(:user, 2) }
+
+      it 'notifies all peers (by creating a notification)' do
+        expect { subject }.to change { Notification.count }.by(2)
+      end
+
+      it 'creates notifications {activity: self, actor: actor, recipient: other commenters, reference: stamp}' do
+        subject
+
+        activity = PublicActivity::Activity.last
+        other_commenters.each do |commenter|
+          notification = Notification.find_by(recipient_id: commenter.id)
+          expect(notification.activity).to eq(activity)
+          expect(notification.actor).to eq(actor)
+          expect(notification.recipient_id).to eq(commenter.id)
+          expect(notification.reference).to eq(stamp)
+        end
+      end
+    end
+  end
 end
