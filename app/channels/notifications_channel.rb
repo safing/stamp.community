f9494919d04524502863cfe41c0fb2f5df8ee53b
davegson <3080765+davegson@users.noreply.github.com>
2019-04-25 11:49:57 +0200

    Stream notifications to current_user

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,9 +1,5 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'notifications'
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stream_for current_user
   end
 end
