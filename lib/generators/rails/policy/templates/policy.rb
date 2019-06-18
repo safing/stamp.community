9bdb57509c396905bfd94791ce12fbc3e5086ee1
davegson <3080765+davegson@users.noreply.github.com>
2019-06-18 14:54:58 +0200

    Add templates for policies & policy specs

--- lib/generators/rails/policy/templates/policy.rb
+++ lib/generators/rails/policy/templates/policy.rb
@@ -1,1 +1,30 @@
-# policy
+<% module_namespacing do -%>
+class <%= class_name %>Policy < ApplicationPolicy
+  attr_reader :user, :<%= singular_table_name %>
+
+  def initialize(user, <%= singular_table_name %>)
+    @user = user
+    @<%= singular_table_name %> = <%= singular_table_name %>
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def update?
+    false
+  end
+
+  def destroy?
+    false
+  end
+end
+<% end -%>
--- lib/generators/rspec/templates/policy_spec.rb
+++ lib/generators/rspec/templates/policy_spec.rb
@@ -1,1 +1,46 @@
-# policy spec
+<% module_namespacing do -%>
+RSpec.describe <%= class_name %>Policy do
+  subject { described_class.new(user, <%= singular_table_name %>) }
+  let(:<%= singular_table_name %>) { FactoryBot.create(:<%= singular_table_name %>) }
+
+  context 'for a visitor' do
+    let(:user) { nil }
+
+    it { is_expected.to forbid_action(:index) }
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+  end
+
+  context 'for a user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    it { is_expected.to forbid_action(:index) }
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+  end
+
+  context 'for a moderator' do
+    let(:user) { FactoryBot.create(:moderator) }
+
+    it { is_expected.to forbid_action(:index) }
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+  end
+
+  context 'for a admin' do
+    let(:user) { FactoryBot.create(:admin) }
+
+    it { is_expected.to forbid_action(:index) }
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action(:destroy) }
+  end
+end
+<% end -%>
