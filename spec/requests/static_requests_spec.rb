548c1f22c6d0bb1678419add83c29e3b5fe29dc9
davegson <3080765+davegson@users.noreply.github.com>
2019-06-20 12:11:16 +0200

    Adapt static request specs

--- spec/requests/static_requests_spec.rb
+++ spec/requests/static_requests_spec.rb
@@ -1,24 +1,14 @@
 RSpec.feature 'static requests', type: :request do
-  describe 'authentication & authourization' do
+  describe 'authentication' do
     describe '#terms' do
       subject(:request) { get tos_path }
 
-      context 'role: guest' do
+      context 'user is unauthenticated (guest)' do
         include_examples 'status code', 200
       end
 
-      context 'role: user' do
+      context 'user is authenticated' do
         include_context 'login user'
-        include_examples 'status code', 200
-      end
-
-      context 'role: moderator' do
-        include_context 'login moderator'
-        include_examples 'status code', 200
-      end
-
-      context 'role: admin' do
-        include_context 'login admin'
         include_examples 'status code', 200
       end
     end
